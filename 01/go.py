151e5f1541a646359693de4a2822c7dc231f130b
Jakub Holak <jakub.holak@nordcloud.com>
2020-12-01 21:13:36 +0100

    Day 01

---
+++ 01/go.py
@@ -0,0 +1,12 @@
+with open("input", "r") as file:
+    fileContents = file.read()
+    numbers = list(map(int, fileContents.splitlines()))
+    for n in numbers:
+        for m in numbers:
+            rest = 2020 - n - m
+            if rest in numbers:
+                print(rest * n * m)
+                break
+        else:
+            continue
+        break
