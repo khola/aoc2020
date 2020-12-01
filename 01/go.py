91b5f16b616cd63d900349abaad10fa8a08b1d59
Jakub Holak <jakub.holak@nordcloud.com>
2020-12-01 21:17:08 +0100

    Fix indents

--- 01/go.py
+++ 01/go.py
@@ -1,12 +1,13 @@
 with open("input", "r") as file:
     fileContents = file.read()
-    numbers = list(map(int, fileContents.splitlines()))
-    for n in numbers:
-        for m in numbers:
-            rest = 2020 - n - m
-            if rest in numbers:
-                print(rest * n * m)
-                break
-        else:
-            continue
-        break
+
+numbers = list(map(int, fileContents.splitlines()))
+for n in numbers:
+    for m in numbers:
+        rest = 2020 - n - m
+        if rest in numbers:
+            print(rest * n * m)
+            break
+    else:
+        continue
+    break
